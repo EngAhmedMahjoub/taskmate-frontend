c9da4954a7f0bfd7a93f84e980f3574e058792c8
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2025-06-08 18:47:56 +1000

    add task service

---
+++ src/services/taskService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_BASE = "http://localhost:5276/api/tasks";
+
+export const getTasks = () => axios.get(API_BASE);
+
+export const createTask = (task) => axios.post(API_BASE, task);
+
+export const updateTask = (id, task) => axios.put(`${API_BASE}/${id}`, task);
+
+export const deleteTask = (id) => axios.delete(`${API_BASE}/${id}`);
