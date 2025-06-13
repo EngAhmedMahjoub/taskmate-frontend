869622c4ec50160eec5092b7fa6e9dfccfaa0163
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2025-06-14 02:37:43 +1000

    define authentication service

---
+++ src/services/authService.js
@@ -0,0 +1,21 @@
+const API = "http://localhost:5276/api/auth";
+
+export const register = async (email, password) => {
+  const res = await fetch(`${API}/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+  if (!res.ok) throw new Error("Registration failed");
+  return await res.json(); // { token }
+};
+
+export const login = async (email, password) => {
+  const res = await fetch(`${API}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+  if (!res.ok) throw new Error("Login failed");
+  return await res.json(); // { token }
+};
